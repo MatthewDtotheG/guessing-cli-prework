0d33cb4b4e66d31ba3343198d29de0fb929fb4d9
Matthew Garrett <mdgarrett1984@gmail.com>
2018-01-27 18:20:01 +0000

    automatically backed up by learn

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -3,7 +3,7 @@ def run_guessing_game
   input = ""
       while input
       input = gets.chomp.to_i
-      random_num = rand(1..6)to_s
+      random_num = rand(1..6).to_s
       if input == random_num
         print "You guessed the correct number!"
         break
