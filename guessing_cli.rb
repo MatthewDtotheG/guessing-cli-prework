6aa57dfd8b2340723c34a926d5c07a0a589e96a2
Matthew Garrett <mdgarrett1984@gmail.com>
2018-01-27 18:53:07 +0000

    automatically backed up by learn

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -2,7 +2,7 @@
 def run_guessing_game
   input = ""
       while input
-        puts "Guess a number between 1 and 6\n"
+        puts "Guess a number between 1 and 6.\n"
       input = gets.downcase.chomp
       random_num = rand(1..6).to_s
       if input.chomp == random_num
