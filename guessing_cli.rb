10caa64ab2bb36a60c722f9b2d33e6f189813fa5
Matthew Garrett <mdgarrett1984@gmail.com>
2018-01-27 18:06:27 +0000

    automatically backed up by learn

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -4,11 +4,11 @@ def run_guessing_game
       while input
       input = gets.chomp.to_i
       random_num = rand(1..6)
-      if input.to_i == random_num
+      if input == random_num
         print "You guessed the correct number!"
-      elsif input.to_i != random_num
+      elsif input != random_num
         print "The computer guessed #{random_num}"
-      elsif input.to_i == "exit"
+      elsif input == "exit"
         break
       end
     break
